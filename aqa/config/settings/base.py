9486bbbb8e1d8a6856007376cf2bbb249b8d8f15
Kevin <kevinsemilla@gmail.com>
2020-08-31 18:10:32 +0800

    created quotation app

--- aqa/config/settings/base.py
+++ aqa/config/settings/base.py
@@ -77,6 +77,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "aqa.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "aqa.users.apps.QuotationConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
