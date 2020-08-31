a1d31ebf52b83ef429788463eaa8538c17266027
Kevin <kevinsemilla@gmail.com>
2020-08-31 17:56:16 +0800

    Started project

---
+++ aqa/aqa/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from aqa.users.models import User
+from aqa.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
