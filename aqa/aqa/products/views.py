cca3a75a72750463f6d98668af993c6758a866ba
Kevin <kevinsemilla@gmail.com>
2020-09-28 03:04:33 +0800

    Changed product update view

--- aqa/aqa/products/views.py
+++ aqa/aqa/products/views.py
@@ -39,35 +39,30 @@ class ProductRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
         return Response(ProductSerializer(product).data, status=status.HTTP_200_OK)
         
     
+    # def update(self, request, pk):
+    #     data = copy.deepcopy(request.data)
+    #     product = Product.objects.filter(pk=pk).first()
+    #     if not product:
+    #         return Response({"error": f"Product code {pk} does not exists"}, status=status.HTTP_400_BAD_REQUEST)
+    #     serializer = ProductSerializer(data=data)
+    #     if serializer.is_valid():
+    #         for attr, value in data.items():
+    #             setattr(product, attr, value)
+    #             product.save()
+    #     return Response({"success": f"Saved changes in product {product.model_name}", "new_data": ProductSerializer(product).data}, status=status.HTTP_200_OK)
+
     def update(self, request, pk):
         data = copy.deepcopy(request.data)
         product = Product.objects.filter(pk=pk).first()
         if not product:
             return Response({"error": f"Product code {pk} does not exists"}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = ProductSerializer(data=data)
-        if serializer.is_valid():
-            for attr, value in data.items():
-                setattr(product, attr, value)
-                product.save()
-        return Response({"success": f"Saved changes in product {product.model_name}", "new_data": ProductSerializer(product).data}, status=status.HTTP_200_OK)
         
+        serializer = ProductSerializer(product, data=data)
 
-
-
-    # def put(self, request, pk):
-    #         data = copy.deepcopy(request.data)
-    #         product = Product.objects.filter(pk=pk).first()
-            
-    #         if not product:
-    #             return Response({"error": f"Product code {pk} does not exists"}, status=400)
-
-    #         product.model_name = data["model_name"] if 'model_name' in data else product.model_name
-    #         product.description = data['description'] if 'description' in data else product.description
-    #         product.capacity = data['capacity'] if 'capacity' in data else product.capacity
-    #         product.sell_price = data['sell_price'] if 'sell_price' in data else product.sell_price
-    #         product.cost_price = data['cost_price'] if 'cost_price' in data else product.cost_price
-    #         product.save()
-    #         return Response({"success": f"Saved changes in product {product.id}"}, status=200)
+        if serializer.is_valid():
+            product_obj = serializer.save()
+            return Response(ProductSerializer(product_obj).data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         product = Product.objects.filter(pk=pk).first()
